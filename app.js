b04b19ab4386723a01dee8e7bfbfbea3db0c0ff0
0wx <callmeoniichandesu@gmail.com>
2020-05-28 10:43:38 +0700

    Add styling to the box

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ const upload = multer({ dest: "public/uploads/" });
 const bodyParser = require("body-parser");
 const rimraf = require("rimraf");
 const express = require("express");
+
+
 const app = new express();
 const rm = rimraf.sync;
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.listen(process.env.PORT || 3000);
 
+
 app.get("/", (req, res) => {
   res.render("upload");
 });
+
 
 app.post("/pdf", upload.array("lampiran"), (req, res) => {
   let data = req.body;
@@ -34,16 +40,7 @@ app.post("/pdf", upload.array("lampiran"), (req, res) => {
         res.send(err);
         console.log(err);
       } else {
-        let options = {
-          height: "33cm",
-          width: "21cm",
-          border: {
-            top: "1cm", // default is 0, units: mm, cm, in, px
-            right: "1cm",
-            bottom: "1cm",
-            left: "1cm",
-          },
-        };
+        let options = require('./kertas.json');
         pdf
           .create(data, options)
           .toFile("./public/uploads/result.pdf", function (err, data) {
@@ -60,12 +57,17 @@ app.post("/pdf", upload.array("lampiran"), (req, res) => {
   );
 });
 
-function f(n) {
-  let x = n.toString().split("");
+
+
+// might be usefull in the future, lol.
+// function to convert 5030422442 into string 5.030.422.442
+
+function parseNum(number) {
+  let arr = number.toString().split("");
   let res = "";
 
-  for (let i = x.length - 1, c = 1; i > -1; i--, c++) {
-    res = x[i] + res;
+  for (let i = arr.length - 1, c = 1; i > -1; i--, c++) {
+    res = arr[i] + res;
     if (c % 3 == 0 && i != 0) res = "." + res;
   }
   return res;
